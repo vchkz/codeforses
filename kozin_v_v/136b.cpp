7e47f0357b46e3797e43e82230da7e4830210a96
vchkoz <vchkoz@mail.ru>
2022-10-09 17:14:53 +0300

    new

--- kozin_v_v/136b.cpp
+++ kozin_v_v/136b.cpp
@@ -1,24 +1,68 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
-//int toTroika(int a) {
-//    while (a > 0) {
-//
-//    }
-//
-//
-//}
+int toTroika(int s, std::vector<int> &vec) {
+    while (s > 0) {
+        vec.push_back(s % 3);
+        s /= 3;
+    }
+    return 0;
+
+
+}
 
 
 int main() {
-    int num, sys, num2 = 0, i;
-    num = 14;
-    sys = 3;
-    for (i = 0; num >= sys - 1; i++) {
-        num2 += (num % sys) * pow(10, i);
-        num /= sys;
+
+
+    int iA;
+    int iC;
+
+    std::cin >> iA;
+    std::cin >> iC;
+
+
+    std::vector<int> va;
+    std::vector<int> vb;
+    std::vector<int> vc;
+
+
+    toTroika(iC, vc);
+    toTroika(iA, va);
+
+
+
+    if (iA > iC) {
+
+        int temp = va.size() - vc.size();
+
+        for (int i = 0; i < temp;  ++i) {
+            vc.push_back(0);
+        }
+
+    } else {
+        int temp = vc.size() - va.size();
+        for (int i = 0; i < temp; ++i) {
+            va.push_back(0);
+        }
+
+
     }
-    std::cout << num2 << " ";
-    num2 += num * pow(10, i++);
-    std::cout << num2 << " " << num;
+
+
+
+
+
+    for (int i = 0; i < vc.size(); ++i) {
+        if (va[i] > vc[i]) { vb.push_back(vc[i] + 3 - va[i]); }
+        else { vb.push_back(vc[i] - va[i]); }
+    }
+
+
+    int res = 0;
+    for (int i = 0; i < vb.size(); ++i) {
+        res += vb[i] * std::pow(3, i);
+    }
+    std::cout << res;
 }
